fdc890a4b08144c1d945c2e1cf0f8ecb53e29f4f
Rebeca <becatriz7@gmail.com>
2020-07-31 22:33:37 -0400

    Adicionado services

---
+++ services/endereco.service.js
@@ -0,0 +1,18 @@
+async function buscarPorCep(cep) {
+  try {
+    let response = await fetch(`https://viacep.com.br/ws/${cep}/json`)
+    let result = await response.json()
+    if (result.erro) throw {
+      message: "CEP não reconhecido",
+      mensagemCliente: "CEP não reconhecido"
+    }
+    return result
+  } catch (erro) {
+    throw {
+      message: `[EnderecoService] Erro ao buscar endereço por CEP: ${erro.message}`,
+      mensagemCliente: erro.mensagemCliente || "Erro ao buscar endereço"
+    }
+  }
+}
+
+export { buscarPorCep }
