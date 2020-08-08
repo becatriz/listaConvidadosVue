28ba866ece0e821281e67e06545fdd9221f8db69
Rebeca <becatriz7@gmail.com>
2020-08-08 10:59:42 -0400

    Adiconando packegae.json

--- services/endereco.service.js
+++ services/endereco.service.js
@@ -1,18 +1,23 @@
-async function buscarPorCep(cep) {
-  try {
-    let response = await fetch(`https://viacep.com.br/ws/${cep}/json`)
-    let result = await response.json()
-    if (result.erro) throw {
-      message: "CEP não reconhecido",
-      mensagemCliente: "CEP não reconhecido"
-    }
-    return result
-  } catch (erro) {
-    throw {
-      message: `[EnderecoService] Erro ao buscar endereço por CEP: ${erro.message}`,
-      mensagemCliente: erro.mensagemCliente || "Erro ao buscar endereço"
+class EnderecoService {
+
+
+ static async buscarPorCep(cep) {
+    try {
+      let response = await fetch(`https://viacep.com.br/ws/${cep}/json`)
+      let result = await response.json()
+      if (result.erro) throw {
+        message: "CEP não reconhecido",
+        mensagemCliente: "CEP não reconhecido"
+      }
+      return result
+    } catch (erro) {
+      throw {
+        message: `[EnderecoService] Erro ao buscar endereço por CEP: ${erro.message}`,
+        mensagemCliente: erro.mensagemCliente || "Erro ao buscar endereço"
+      }
     }
   }
 }
 
-export { buscarPorCep }
+
+export default EnderecoService
